b15717ed96eb845a7e8e382d6fbe737b6b0a5f5f
EDCG1995 <b00129517@mytudublin.ie>
2021-03-05 21:45:00 +0000

    .

--- layout/header.php
+++ layout/header.php
@@ -2,5 +2,14 @@
     <head>
     <meta charset="utf-8">
     <title>"Flight.ie: Because you deserve it"</title>
+    <link href="../css/base.css" rel="stylesheet">
     </head>
     <body>
+        <header class="header">
+            <div class ="logo">'logo placeholder'</div>
+            <div class="header_title">About us</div>
+            <div class="header_title">Book a flight</div>
+            <div class="header_title">First class club</div>
+            <div class="header_title">Log in | Set up</div>
+            <div class="header_title">EN 'flag placeholder'</div>
+        </header>
